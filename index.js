fa207adcad76b81df024040ccc6e43bf7c7ec8ca
Jed Borovik <mr.borovik@gmail.com>
2015-04-20 13:43:28 -0700

    Fix formatting in index.js

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ exports.fromCharToNumber = function(val) {
 }
 
 function cast(value) {
-	var intValue = ('string' == typeof value) ? parseInt(value, 2) : value;
-	return {
-		from: function from(fromType) {
+  var intValue = ('string' == typeof value) ? parseInt(value, 2) : value;
+  return {
+    from: function from(fromType) {
       if (fromType == 'char') rangeAssert(intValue, 0, 255);
-			return {
-				to: function to(toType) {
-					if (fromType == 'char' && toType == 'number') {
-						return (intValue > 127) ? intValue - 256 : intValue;
-					}
+      return {
+        to: function to(toType) {
+          if (fromType == 'char' && toType == 'number') {
+            return (intValue > 127) ? intValue - 256 : intValue;
+          }
           throw Error('Not implemented');
-				}
-			}
+        }
+      }
 		}
-	}
+  }
 }
 
 function rangeAssert(val, min, max) {
