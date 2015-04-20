0ac79e15aaabdda8a79bac3452b082ebd1729193
Jed Borovik <mr.borovik@gmail.com>
2015-04-20 13:36:37 -0700

    Fix positive number bug in char to number

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ function cast(value) {
 	var intValue = ('string' == typeof value) ? parseInt(value, 2) : value;
 	return {
 		from: function from(fromType) {
+      if (fromType == 'char') rangeAssert(intValue, 0, 255);
 			return {
 				to: function to(toType) {
 					if (fromType == 'char' && toType == 'number') {
-						return intValue - 256;
+						return (intValue > 127) ? intValue - 256 : intValue;
 					}
           throw Error('Not implemented');
 				}
 			}
 		}
 	}
+}
+
+function rangeAssert(val, min, max) {
+  if (val < min || val > max) throw Error(val + ' out of bounds');
 }
--- test/test.js
+++ test/test.js
@@ -6,8 +6,22 @@ describe('cast', function() {
 		describe('to number', function() {
 			it('should handle negative inputs', function() {
 				var result = cast('10011100').from('char').to('number');
-				assert.equal(result, -100);
+				assert.equal(-100, result);
 			});
+      it('should handle 0', function() {
+        var result = cast('00000000').from('char').to('number');
+        assert.equal(0, result);
+      });
+      it('should handle positive inputs', function() {
+        var result = cast('01100100').from('char').to('number');
+        assert.equal(100, result);
+      })
 		});
 	});
 });
+
+describe('fromCharToNumber', function() {
+  it('should handle negative inputs', function() {
+    assert.equal(-100, cast.fromCharToNumber('10011100'));
+  });
+})
